f4b45ef832fc47dffb1949abe9998c344c5b083f
avithal <avithal@gmail.com>
2018-01-24 16:20:10 +0200

    the perfect code

--- ModuleCheck.py
+++ ModuleCheck.py
@@ -1,7 +1,7 @@
 """ Checking modules in a  system"""
 import sphinx
 import sys
-print (sphinx.__version__ )
-modulename = "numpy"
-if modulename not in sys.modules:
-   print("You have not imported the {} module:" + modulename)
+print(sphinx.__version__)
+module_name = "numpy"
+if module_name not in sys.modules:
+    print("You have not imported the {} module:" + module_name)
